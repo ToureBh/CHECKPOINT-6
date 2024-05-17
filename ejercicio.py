740507827089fd4a78cf6917ac0d66ea07b54f5d
ToureBh <149252642+ToureBh@users.noreply.github.com>
2024-05-17 13:49:01 +0200

    Delete ejercicio.py

--- ejercicio.py
+++
@@ -1,8 +0,0 @@
-
-class Usuario:
-    def __init__(self, nombre_usuario, contraseña):
-        self.nombre_usuario = nombre_usuario
-        self.contraseña = contraseña
-
-
-margarita = Usuario("Margarita1999", "1234567ABC")
